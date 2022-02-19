81566a6bc9dba7d04c8aaa2e227603ec01df4108
Bentarraya <bentarraya01@gmail.com>
2022-02-19 08:31:37 +0700

    adding social media icon

--- main.js
+++ main.js
@@ -30,5 +30,5 @@ const showMenu = (toggleId, navId) => {
   gsap.from(".nav__item", { opacity: 0, duration: 3, delay: 3.2, y: 25, ease: "expo.out", stagger: 0.2 });
   
   // SOCIAL
-  gsap.from(".home__social-icon", { opacity: 0, duration: 3, delay: 4, y: 25, ease: "expo.out", stagger: 0.2 });
+  gsap.from(".asm", { opacity: 0, duration: 3, delay: 4, y: 25, ease: "expo.out", stagger: 0.2 });
   
